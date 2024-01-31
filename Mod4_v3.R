743f31be236e1201020b42f7a15637ab6ecbe622
ProfessorHatsuneMiku <143456189+ProfessorHatsuneMiku@users.noreply.github.com>
2024-01-31 11:50:36 -0500

    LIS 4370 Module 4 Assignment - Take 2

---
+++ Mod4_v3.R
@@ -0,0 +1,68 @@
+# LIS4370 R Programming - Module 4
+# Kathryn Burkhart
+
+# The following data is from a local hospital.
+# The data set contains 5 variables based on the observation of 8 patients
+# Additionally, the data displays the measurement of blood pressure, 
+# first assessment by general doctor (bad=1, good =0) titled "first",
+# the second assessment by external doctor (called "second"), 
+# and the last row provides the head of the emergency unit's decision regarding 
+# immediate care for the patient based on the values 0 or 1 (low = 0, high =1).
+# The names of your variables are as follows: 
+# "Freq","bloodp","first”, " second”, ”finaldecision”
+
+# Step 1: insert data entries into vectors under the appropriate names and 
+# put them into a data frame.
+
+Freq <- c(0.6, 0.3, 0.4, 0.4, 0.2, 0.6, 0.3, 0.4, 0.9, 0.2)
+
+bloodp <- c(103, 87, 32, 42, 59, 109, 78, 205, 135, 176)
+
+# General Doctor Assessment: Bad = 1, Good = 0
+first <- c(1, 1, 1, 1, 0, 0, 0, 0, "NA", 1)
+
+# External Doctor Assessment: Low = 0, High = 1
+second <- c(0, 0, 1, 1, 0, 0, 1, 1, 1, 1)
+
+# Emergency unit decision: Low = 0, High = 1
+finaldecision <- c(0, 1, 0, 1, 0, 1, 0, 1, 1, 1)
+
+# Patient Info data frame
+patientInfo <- data.frame(Freq, bloodp, first, second, finaldecision)
+
+# Make sure first is numeric
+patientInfo$first <- as.numeric(patientInfo$first)
+
+# Load necessary packages
+library(ggplot2)
+
+# boxplot
+boxplot(patientInfo)
+
+# Individual histograms
+# Freq
+ggplot(patientInfo, aes(x = Freq)) +
+  geom_histogram(binwidth = 1, fill = "blue", color = "black", alpha = 0.7) +
+  ggtitle("Frequency of Visit Histogram")
+
+# bloodp
+ggplot(patientInfo, aes(x = bloodp)) +
+  geom_histogram(binwidth = 1, fill = "green", color = "black", alpha = 0.7) +
+  ggtitle("Blood Pressure Histogram")
+
+# first
+ggplot(patientInfo, aes(x = first)) +
+  geom_histogram(binwidth = 1, fill = "red", color = "black", alpha = 0.7) +
+  ggtitle("First Decision by General Doctor Histogram")
+
+# second
+ggplot(patientInfo, aes(x = second)) +
+  geom_histogram(binwidth = 1, fill = "purple", color = "black", alpha = 0.7) +
+  ggtitle("Second Decision by External Doctor Histogram")
+
+# finaldecision
+ggplot(patientInfo, aes(x = finaldecision)) +
+  geom_bar() +
+  ggtitle("Final Decision by Head of Emergency Unit Histogram") +
+  xlab("Final Decision") +
+  ylab("Frequency")
